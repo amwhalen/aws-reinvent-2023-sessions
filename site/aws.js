832402e5d51677300760bd0cb5d78ab46530fa8b
Andrew Whalen <awhalen@artemis.localdomain>
2023-09-26 21:45:06 -0400

    add calendar for favorites

--- site/aws.js
+++ site/aws.js
@@ -15,11 +15,31 @@ var configuration = {
       size: 2,
       conjunction: true,
     },
+    aws_tag_day: {
+      title: 'Day',
+      size: 10,
+      conjunction: true,
+    },
+    startTimeNice: {
+      title: 'Start Time',
+      size: 100,
+      conjunction: true,
+    },
+    venueName: {
+      title: 'Venue',
+      size: 100,
+      conjunction: true,
+    },
     trackName: {
       title: 'Session Type',
       size: 10,
       conjunction: true,
     },
+    // startDateTime: {
+    //   title: 'Start Date Time',
+    //   size: 10,
+    //   conjunction: true,
+    // },
     aws_tag_topic: {
       title: 'Topic',
       size: 100,
@@ -49,6 +69,37 @@ var configuration = {
   }
 }
 
+function timeConverter(UNIX_timestamp) {
+  var three_hours = 3 * 60 * 60;
+  var a = new Date((UNIX_timestamp-three_hours) * 1000);
+  var year = a.getFullYear();
+  var m = ('0' + (a.getMonth()+1)).slice(-2);
+  var d = ('0' + a.getDate()).slice(-2);
+  var date = a.getDate();
+  var hour = ('0' + a.getHours()).slice(-2);
+  var min = ('0' + a.getMinutes()).slice(-2);
+  var sec = ('0' + a.getSeconds()).slice(-2);
+  // Desired output: 2023-11-27T09:00:00
+  var dt = year + '-' + m + '-' + d + 'T' + hour + ':' + min + ':' + sec;
+  return dt;
+}
+
+function startTimeConverter(UNIX_timestamp) {
+  var three_hours = 3 * 60 * 60;
+  var a = new Date((UNIX_timestamp-three_hours) * 1000);
+  var hour = a.getHours();
+  var min = ('0' + a.getMinutes()).slice(-2);
+
+  var h = (hour > 12) ? (hour-12) : hour;
+  var h = (h == 0) ? 12 : h;
+  var suffix = (hour >= 12) ? 'pm' : 'am';
+  
+  var dt = h + suffix;
+  
+  // Desired output: 1pm
+  return dt;
+}
+
 Vue.component('paginate', VuejsPaginate);
 const router = new VueRouter();
 
@@ -73,6 +124,7 @@ var vm = new Vue({
     var lastChanged = this.lastChanged || Date.now();
     var generated_datetime = this.generated_datetime || null;
     var information = this.information || null;
+    var calendar = this.calendar || null;
     var hiddenFacets = this.hiddenFacets || [];
     var config = configuration;
 
@@ -89,6 +141,7 @@ var vm = new Vue({
       viewMode: viewMode,
       lastChanged: lastChanged,
       hiddenFacets: hiddenFacets,
+      calendar: calendar,
       config: config
     }
   },
@@ -119,18 +172,65 @@ var vm = new Vue({
         v.information += ' AWS session data last refreshed on ' + v.generated_datetime + ' UTC.';
         v.information += ' More info at https://github.com/amwhalen/aws-reinvent-2023-sessions.';
 
-        // pull in any favorites stored in local storage
+        
         v.rows.map(function(row) {
+          
+          // pull in any favorites stored in local storage
           key = "favorite_"+row.thirdPartyID;
           if (localStorage.getItem(key) !== null && localStorage.getItem(key) == 'Yes') {
             row.my_favorites = 'Yes';
           }
+          
+          // A bunch of other engineering of the data to make it nice
+          if (row.startDateTime != '') {
+            row.start = timeConverter(row.startDateTime);
+            row.startTimeNice = startTimeConverter(row.startDateTime);
+            row.endTimeNice = startTimeConverter(row.endDateTime);
+          }
+          if (row.endDateTime != '') {
+            row.end = timeConverter(row.endDateTime);
+          }
+          if ('aws_tag_day' in row && row.aws_tag_day.length > 0) {
+            row.day = row.aws_tag_day.join(', ');
+          } else {
+            row.day = '';
+          }
+          row.wide_title = row.title;
+          
+          var venueInitials;
+          if (row.venueName != '') {
+            venueInitials = row.venueName.split(" ").map((n)=>n[0]).join("");
+          } else {
+            venueInitials = 'TBD';
+          }
+          row.title = venueInitials + ': ' + row.title;
+          
+          // set event background color based on location:
+          var venueColors = {
+            'MGM Grand': '#198754',
+            'Wynn': '#0d6efd',
+            'Caesars Forum': '#dc3545',
+            'Mandalay Bay': '#e4ab00',
+            'Venetian': '#7f1a8c',
+          };
+          if (row.venueName != '' && row.venueName in venueColors) {
+            row.backgroundColor = venueColors[row.venueName]; 
+          } else {
+            row.backgroundColor = '#666666';
+          }
+          
           return row;
         });
 
         // set the Items JS database with the rows and config
         v.db = itemsjs(v.rows, configuration);
         v.isLoaded = true;
+
+        if (v.viewMode == 'grid') {
+          v.setViewModeGrid();
+        } else {
+          v.setViewModeWide();
+        }
 
       }).fail(function (jqXHR, textStatus) {
         v.isLoaded = true;
@@ -142,6 +242,94 @@ var vm = new Vue({
 
   },
   methods: {
+    setViewModeGrid: function() {
+      
+      var v = this;
+      
+      v.viewMode = 'grid';
+      
+      // show only favorites in calendar view
+      var cal_events = v.db.search({
+        per_page: 2000,
+        sort: 'id_desc',
+        filters: {
+          'my_favorites': ['Yes']
+        }
+      });
+        
+      // calendar view
+      var calendarEl = document.getElementById('calendar');
+      //console.log('calendarEl', document.getElementById('calendar'));
+      v.calendar = new FullCalendar.Calendar(document.getElementById('calendar'), {
+        initialView: 'timeGridFourDay',
+        views: {
+          timeGridFourDay: {
+            type: 'timeGrid',
+            duration: { days: 5 }
+          }
+        },
+        initialDate: '2023-11-27',
+        slotMinTime: '07:00:00',
+        slotMaxTime: '20:00:00',
+        height: 'auto',
+        contentHeight: 'auto',
+        events: cal_events.data.items,
+        displayEventEnd: false,
+        displayEventTime: false,
+        headerToolbar: {
+          left: '',
+          center: '',
+          right: ''
+        },
+        eventTimeFormat: {
+          // DayGrid views. '7p'
+          hour: 'numeric',
+          minute: '2-digit',
+          omitZeroMinute: true,
+          meridiem: 'narrow'
+        },
+        eventDidMount: function(info) {
+          // popover on click of an event
+          $(info.el).popover({
+            'title': info.event.extendedProps.wide_title,
+            'content': '<strong>' + info.event.extendedProps.startTimeNice + ' @ '+info.event.extendedProps.venueName+'</strong><p>'+info.event.extendedProps.description+'</p>',
+            'html': true,
+            'placement': 'top',
+          });
+        },
+        // events: [
+        //   {
+        //     title: 'Event',
+        //     start: '2023-11-27T09:00:00',
+        //     end: '2023-11-27T10:00:00'
+        //   },
+        // ]
+      });
+      v.calendar.render();
+
+      // Enable bootstrap popovers
+      var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+      var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+        return new bootstrap.Popover(popoverTriggerEl)
+      });
+      
+    },
+    setViewModeWide: function() {
+      var v = this;
+      v.viewMode = 'wide';
+      
+      // delete calendar
+      v.calendar = null;
+      //console.log('calendar to delete', document.getElementById('calendar'));
+      document.getElementById('calendar').remove();
+      
+      // create new container for calendar
+      var innerDiv = document.createElement('div');
+      innerDiv.setAttribute('id', 'calendar');
+      document.getElementById('calendar-container').appendChild(innerDiv);
+      
+      //console.log('calendar recreated', document.getElementById('calendar'));
+    },
     // Clear all search filters.
     reset: function () {
       var filters = {};
@@ -272,14 +460,19 @@ var vm = new Vue({
       var v = this;
 
       var csv_rows = [
-        ['ID','Type','Title','Description']
+        ['ID','Day','Start Time','End Time','Venue','Location','Type','Title','Description']
       ];
       v.rows.forEach(function(row) {
         if (row.my_favorites == 'Yes') {
           csv_rows.push([
             row.thirdPartyID,
+            row.aws_tag_day,
+            row.startTimeNice,
+            row.endTimeNice,
+            row.venueName,
+            row.locationName,
             row.trackName,
-            '"' + row.title + '"',
+            '"' + row.wide_title + '"',
             '"' + row.description + '"'
           ]);
         }
@@ -429,4 +622,4 @@ var vm = new Vue({
 var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
 var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
   return new bootstrap.Popover(popoverTriggerEl)
-})
+});
